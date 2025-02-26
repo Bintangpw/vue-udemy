70ef32a96061d879977bfd098852dd9a33409563
Bintang Putro <bntngputro@gmail.com>
2025-02-27 06:04:12 +0700

    assignment 14

---
+++ assignment 14/src/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <header>
+                <h1>Server Status</h1>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ assignment 14/src/ServerDetails.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="col-12 col-sm-6">
+        <p>Server Details are currently not updated</p>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
